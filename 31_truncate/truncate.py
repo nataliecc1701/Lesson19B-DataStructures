668e5298ac531ffbbbac7fffd89673258cdaeeb7
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-17 00:06:02 -0800

    31 done and dusted

--- 31_truncate/truncate.py
+++ 31_truncate/truncate.py
@@ -24,3 +24,9 @@ def truncate(phrase, n):
         >>> truncate("Woah", 3)
         '...'
     """
+    
+    if n < 3:
+        return 'Truncation must be at least 3 characters.'
+    if len(phrase) >= n:
+        return phrase[:n-3] + "..."
+    return phrase
