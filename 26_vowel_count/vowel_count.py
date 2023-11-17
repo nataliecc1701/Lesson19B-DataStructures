eea00bde481e822769c3cda5ad22cbbb5f4f48d8
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-16 23:31:18 -0800

    26 done! 2/3!

--- 26_vowel_count/vowel_count.py
+++ 26_vowel_count/vowel_count.py
@@ -7,3 +7,11 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    vowels = "aeiou"
+    
+    freq_map = {}
+    for char in phrase.lower():
+        if char in vowels:
+            freq_map[char] = freq_map.get(char,0) + 1
+    
+    return freq_map
