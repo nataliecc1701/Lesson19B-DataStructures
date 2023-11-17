48c05ad428b4d57702b205f48a26c128b63ee6ad
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-17 01:04:47 -0800

    FINISHED with all standard exercises

--- 39_find_greater_numbers/find_greater_numbers.py
+++ 39_find_greater_numbers/find_greater_numbers.py
@@ -19,3 +19,10 @@ def find_greater_numbers(nums):
         >>> find_greater_numbers([])
         0
     """
+    count = 0
+    for pair in enumerate(nums):
+        for n in nums[pair[0]:]:
+            if n > pair[1]:
+                count += 1
+                
+    return count
