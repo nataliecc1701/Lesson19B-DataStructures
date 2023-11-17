23fcb91fe264d9aa7a1d7e215aabdd29cf531f1f
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-16 21:38:03 -0800

    solved 7

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,13 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    
+    letters_dict = {}
+    for char in phrase:
+        if char.isalpha():
+            if char not in letters_dict:
+                letters_dict[char] = 1
+            else:
+                letters_dict[char] += 1
+            
+    return letters_dict
