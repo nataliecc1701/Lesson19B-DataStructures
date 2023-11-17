0a643599b7943ee74d8b035bbcb57d62c813c2d3
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-17 00:31:25 -0800

    34 down, 5 to go

--- 34_same_frequency/same_frequency.py
+++ 34_same_frequency/same_frequency.py
@@ -1,3 +1,13 @@
+def frequency_map(iterable):
+    """Return frequency map over iterable.
+    called in same_frequency. copied and modified from exercise 7
+    """
+    freq_map = {}
+    for entry in iterable:
+        freq_map[entry] = freq_map.get(entry, 0) + 1
+            
+    return freq_map
+
 def same_frequency(num1, num2):
     """Do these nums have same frequencies of digits?
     
@@ -10,3 +20,7 @@ def same_frequency(num1, num2):
         >>> same_frequency(1212, 2211)
         True
     """
+    s1 = str(num1)
+    s2 = str(num2)
+    
+    return frequency_map(s1) == frequency_map(s2)
