13519ca86987e1b767ea2782268803b4c02a8488
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-17 00:47:30 -0800

    better solution to 36

--- 36_find_the_duplicate/find_the_duplicate.py
+++ 36_find_the_duplicate/find_the_duplicate.py
@@ -1,3 +1,13 @@
+def frequency_map(iterable):
+    """Return frequency map over iterable.
+    this is the same one as I made for problem 34
+    """
+    freq_map = {}
+    for entry in iterable:
+        freq_map[entry] = freq_map.get(entry, 0) + 1
+            
+    return freq_map
+
 def find_the_duplicate(nums):
     """Find duplicate number in nums.
 
@@ -13,3 +23,16 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+    
+    # inefficient way
+    # freq_map = frequency_map(nums)
+    # for item in freq_map.items():
+    #     if item[1] > 1:
+    #         return item[0]
+    # return None
+    
+    # better way
+    for num in nums:
+        if nums.count(num) == 2:
+            return num
+    return None
