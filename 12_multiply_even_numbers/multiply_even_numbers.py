ca4808e8aac0230d87f37fdebd880f28794215a3
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-16 22:10:22 -0800

    solved 12

--- 12_multiply_even_numbers/multiply_even_numbers.py
+++ 12_multiply_even_numbers/multiply_even_numbers.py
@@ -12,3 +12,9 @@ def multiply_even_numbers(nums):
         >>> multiply_even_numbers([1, 3, 5])
         1
     """
+    
+    prod = 1
+    evens = [num for num in nums if num % 2 == 0]
+    for even in evens:
+        prod *= even
+    return prod
