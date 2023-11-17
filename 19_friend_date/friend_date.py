db3b34211e3d61372119eb6ad1256c1fb48c3224
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-16 23:02:21 -0800

    solved 19

--- 19_friend_date/friend_date.py
+++ 19_friend_date/friend_date.py
@@ -16,3 +16,8 @@ def friend_date(a, b):
         >>> friend_date(sauron, gandalf)
         True
     """
+    
+    a_hobbies = set(a[2])
+    b_hobbies = set(b[2])
+    
+    return bool(a_hobbies & b_hobbies)
